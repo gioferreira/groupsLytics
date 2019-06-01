74600984587f759fd46ecd2a3f1aee232746c1fa
Giovani Ferreira <giovaniferreira@Giovanis-MacBook-Pro.local>
2019-06-01 06:49:17 -0300

    refactor da madrugada

--- seleniumFBScraper.R
+++ seleniumFBScraper.R
@@ -1,7 +1,7 @@
 library(RSelenium)
 library(tidyverse)
 library(magrittr)
-
+source("src/utils/not_in.R")
 
 # Start Session ####
 rD <- rsDriver(port = 4567L, 
@@ -9,11 +9,6 @@ rD <- rsDriver(port = 4567L,
                 chromever = "74.0.3729.6", 
                 verbose = FALSE)
 remDr <- rD$client
-
-# Define Login Vars ####
-login_url <- "http://www.facebook.com"
-my_email <- list("giovaniferreira")
-my_pass <- list("CaraAmassada1")
 
 # Login ####
 loginFB <- function(remDr, login_url, my_email, my_pass) {
@@ -29,143 +24,207 @@ loginFB <- function(remDr, login_url, my_email, my_pass) {
   logInBtn$clickElement()
 }
 
+login_url <- "http://www.facebook.com"
+my_email <- read_rds("data/my_email.rds")
+my_pass <- read_rds("data/my_pass.rds")
+
 loginFB(remDr, login_url, my_email, my_pass)
 
-# Define Group Vars ####
-group_url <- "https://www.facebook.com/groups/910141242384839/?sorting_setting=CHRONOLOGICAL"
-
 # Go to group ####
-remDr$navigate(group_url)
+openGroup <- function(group_id, sorting = "CHRONOLOGICAL"){
+  group_url <- paste0( "https://www.facebook.com/groups/", 
+                       group_id, 
+                       "/?sorting_setting=", 
+                       sorting)
+  remDr$navigate(group_url)
+}
 
-get_texts <- function(post) {
+group_id <- read_rds("data/group_id.rds")
+openGroup(group_id)
+
+find_element <- function(remDr, using, value){
+  suppressMessages({
+    tryCatch(remDr$findElement(using = using, 
+                               value = value), 
+             error = function(e){
+               return(NA)
+             })
+  })
+}
+
+find_element(remDr, "css", "ahdljk")
+
+make_get_post <- function(type = "text") {
+  if (type %!in% c("text", "url")) {
+    stop('Unknown Type')
+  }
+  get_function <- function(post) {
+    out <- tryCatch(
+      {
+        if (type == "text") {
+          element <- post$getElementText()
+        } else if (type == "url") {
+          element <- post$getElementAttribute("href")
+        }
+        element
+      },
+      error = function(cond) {
+        return(NULL)
+      },
+      warning = function(cond) {
+        message("Warning")
+        message(cond)
+        return(NA)
+      },
+      finally = {
+      }
+    )    
+    return(out)    
+  }
+}
+make_get_remDr <- function(using = "css", value, type = "text", method = "one") {
+  if (type %!in% c("text", "url")) {
+    stop('Unknown Type')
+  }
+  if (method %!in% c("one", "all")) {
+    stop('Method must be either "one" or "all"')
+  }
+  
+  get_function <- function(remDr) {
+    out <- tryCatch(
+      {
+        get_elem <- make_get_post(type)
+        if (method == "one") {
+          # element <- remDr$findElement(using = using, value = value)
+          element <- find_element(remDr, using, value)
+          if (!is.na(element)) {
+            element <- get_elem(element) 
+          }
+        } else if (method =="all") {
+          elements <- remDr$findElements(using = using, value = value)
+          element <- sapply(elements, get_elem)
+        }
+        
+        # if (length(element) == 1) {} # achei errado
+          element <- unlist(element)
+        
+        element
+      },
+      error = function(cond) {
+        return(NA)
+      },
+      warning = function(cond) {
+        message("Warning")
+        message(cond)
+        return(NA)
+      }
+    )    
+    return(out)    
+  }
+}
+
+
+# Get all permalinks and summary texts on page ####
+get_permalinks <- make_get_remDr(using = "css", 
+                                 value = "._5pcq", 
+                                 type = "url", 
+                                 method = "all")
+get_sumr_texts <- make_get_remDr(using = "css", 
+                                 value = "[data-testid='post_message']", 
+                                 method = "all")
+
+posts_tbl <- tibble(sumrText = get_sumr_texts(remDr),
+                    permalink = get_permalinks(remDr))
+
+process_post <- function(remDr, link) {
   out <- tryCatch(
     {
-      unlist(post$getElementText())
+      get_author_name <- make_get_remDr(using = "css", 
+                                        value = '.fwb  [ajaxify*="member_bio"]',
+                                        type = "text")
+      get_author_link <- make_get_remDr(using = "css", 
+                                        value = '.fwb  [ajaxify*="member_bio"]',
+                                        type = "url")
+      get_interactions <- make_get_remDr(using = "css", 
+                                         value = "._81hb",
+                                         type = "text")
+      get_comments_counter <- make_get_remDr(using = "css", 
+                                             value = '[data-testid*="CommentsCount"',
+                                             type = "text")
+      get_post_message <- make_get_remDr(using = "css", 
+                                         value = '[data-testid*="post_message"',
+                                         type = "text")
+      
+      mainWindow <- unlist(remDr$getCurrentWindowHandle())
+      script <- paste0('window.open("', link, '", "windowName", "height=768,width=1024");')
+      remDr$executeScript(script)
+      newWindow <- remDr$getWindowHandles()[[2]]
+      remDr$switchToWindow(newWindow)
+      
+      authorName <- get_author_name(remDr)
+      authorLink <- get_author_link(remDr)
+      interactionsCounter <- get_interactions(remDr)
+      commentsCounter <- get_comments_counter(remDr)
+      postMessage <- get_post_message(remDr)
+      
+      tbl <- tibble(authorName = authorName,
+                    authorLink = authorLink,
+                    interactionsCounter = interactionsCounter,
+                    commentsCounter = commentsCounter,
+                    postMessage = postMessage)
+      
+      remDr$closeWindow()
+      remDr$switchToWindow(mainWindow)
+      tbl
     },
     error=function(cond) {
       message("Error")
       message(cond)
-      return(NA)
+      return(NULL)
     },
     warning=function(cond) {
       message("Warning")
       message(cond)
-      return(NULL)
-    },
-    finally={
-    }
-  )    
-  return(out)
-}
-get_permalink <- function(post) {
-  out <- tryCatch(
-    {
-      unlist(post$getElementAttribute("href"))
-    },
-    error=function(cond) {
-      message("Error")
-      message(cond)
       return(NA)
-    },
-    warning=function(cond) {
-      message("Warning")
-      message(cond)
-      return(NULL)
-    },
-    finally={
     }
   )    
   return(out)
 }
 
-postsLinks <- remDr$findElements(using = "css", value = "._5pcq")
-postsTexts <- remDr$findElements(using = "css", value = "[data-testid='post_message']")
 
-posts_tbl <- tibble(sumrTexts = character(),
-                     permalink = character())
+link <- posts_tbl[1,'permalink']
+process_post(remDr, link)
+
+
+
+
+
 
 tmp_main <- tibble(sumrTexts = sapply(postsTexts, get_texts),
                    permalink = sapply(postsLinks, get_permalink))
 
 
 
-get_poster <- function(post){
-  out <- tryCatch(
-    {
-      unlist(post$getElementText())
-    },
-    error=function(cond) {
-      message("Error")
-      message(cond)
-      return(NA)
-    },
-    warning=function(cond) {
-      message("Warning")
-      message(cond)
-      return(NULL)
-    },
-    finally={
-    }
-  )    
-  return(out)  
-}
 
 
 
-open_link_nw <- function(post) {
-  out <- tryCatch(
-    {
-      link <- unlist(post$getElementAttribute("href"))
-      mainWindow <- remDr$getCurrentWindowHandle()
-      script <- paste0('window.open("', link, '", "windowName", "height=768,width=1024");')
-      remDr$executeScript(script)
-      newWindow <- remDr$getWindowHandles()[[2]]
-      remDr$switchToWindow(newWindow)
-      postPoster <- remDr$findElement(using = "css", value = "._q7o .profileLink")
-      authorProfile <- postPoster$getElementText()
-      authorName <- postPoster$getElementText()
-      remDr$closeWindow()
-      remDr$switchToWindow(mainWindow[[1]])
-      tbl <- tibble(authorName = authorName[[1]],
-                    authorProfile = authorProfile[[1]])
-      tbl
-    },
-    error=function(cond) {
-      message("Error")
-      message(cond)
-      return(NA)
-    },
-    warning=function(cond) {
-      message("Warning")
-      message(cond)
-      return(NULL)
-    },
-    finally={
-    }
-  )    
-  return(out)
-}
 
 
 
-post <- postsLinks[[1]]
-open_link_nw(post)
+post <- postsLinks[[3]]
+process_post(post)
 
-sapply(postsLinks, open_link_nw)
+tbl <- bind_rows(lapply(postsLinks, process_post))
 
-#open each link in new window
-#scrape information from each tab
-#close each tab
-#return to main tab
+tmp_main %>%
+  bind_cols(tbl)
+
 #scroll down
 #get new links and sumr texts, discarding old ones
 #repeat
 
 posts_tbl %<>%
-  bind_rows(temp_tbl)
-
-remDr$win
-
+  bind_rows(tmp_main)
 
 
 remDr$close()
